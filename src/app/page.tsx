36b6a4a91705755db68b69a93b8b7a99f194f0c2
Miho M <30137645+rami6@users.noreply.github.com>
2024-01-18 21:30:04 +0100

    Add introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { Dancing_Script } from 'next/font/google'
+import { Dancing_Script, Alata } from 'next/font/google'
 
 const dancingScript = Dancing_Script({ subsets: ['latin'] })
+const alata = Alata({ weight: '400', preload: false })
 
 export default function Home() {
-  const logoClassName = `logo ${dancingScript.className} mt-4 text-5xl text-center`
+  const logoClasses = `logo ${dancingScript.className} mt-8 text-5xl text-center`
+  const introClasses = `intro ${alata.className} mt-20 text-2xl text-center`
 
   return (
     <main>
       <div className="container mx-auto max-w-lg p-2">
-        <h1 className={logoClassName}>Taste Tango</h1>
+        <h1 className={logoClasses}>Taste Tango</h1>
+        <div className={introClasses}>What do you have?</div>
       </div>
     </main>
   )
