1242f0694054481b1cd576973790750d5eeaeb58
Miho M <30137645+rami6@users.noreply.github.com>
2024-01-18 21:14:30 +0100

    Add the logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Dancing_Script } from 'next/font/google'
+
+const dancingScript = Dancing_Script({ subsets: ['latin'] })
+
 export default function Home() {
+  const logoClassName = `logo ${dancingScript.className} mt-4 text-5xl text-center`
+
   return (
     <main>
-      <div className="container mx-auto max-w-lg p-2">To be updated</div>
+      <div className="container mx-auto max-w-lg p-2">
+        <h1 className={logoClassName}>Taste Tango</h1>
+      </div>
     </main>
   )
 }
